ba507984005cf3ed9d7e3af6d8dc6b365c2c6d2e
Eduardo Andrade <eduardo.andrade15@hotmail.com>
2025-08-28 18:32:12 -0300

    add collect coverage from on jest config

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,18 @@ module.exports = {
   collectCoverage: true,
   testEnvironment: "node",
   coverageDirectory: "coverage",
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx,ts,tsx}",
+    "!src/index.ts"
+  ],
+  // coverageThreshold: {
+  //   global: {
+  //     branches: 90,
+  //     functions: 90,
+  //     lines: 90,
+  //     statements: 90
+  //   }
+  // },
   transform: {
     ...tsJestTransformCfg,
   },
