e2cc0d2f99aae44e5bb7b3a42b42632089262f40
Eduardo Andrade <eduardo.andrade15@hotmail.com>
2025-08-28 19:01:13 -0300

    do some operations

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,50 @@
-console.log("olar");
+import { PrismaClient } from "../generated/prisma";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const usersWithPosts = await prisma.user.findMany({
+    include: {
+      posts: true,
+    },
+  });
+  console.dir(usersWithPosts, { depth: null });
+  //   const user = await prisma.user.create({
+  //     data: {
+  //       name: "Bob",
+  //       email: "bob@prisma.io",
+  //       posts: {
+  //         create: [
+  //           {
+  //             title: "Hello World",
+  //             published: true,
+  //           },
+  //           {
+  //             title: "My second post",
+  //             content: "This is still a draft",
+  //           },
+  //         ],
+  //       },
+  //     },
+  //   });
+  //   console.log(user);
+  //   const users = await prisma.user.findMany();
+  //   console.log(users);
+  //   const user = await prisma.user.create({
+  //     data: {
+  //       name: "Alice",
+  //       email: "alice@prisma.io",
+  //     },
+  //   });
+  //   console.log(user);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
